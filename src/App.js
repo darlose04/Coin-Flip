bf20b0a879b08d86a687a841d0f55c7c77ca2811
Zach Smith <zsmith126028@gmail.com>
2019-06-01 14:13:57 -0500

    add CoinContainer to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import CoinContainer from './components/CoinContainer';
+import Coin from './components/Coin';
 
 function App() {
   return (
     <div className="App">
-      
+      <CoinContainer />
     </div>
   );
 }
